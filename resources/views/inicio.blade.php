0a8005dbce6b383e334d70ccd7d3210ce4eba03f
Danx398 <danidannniel398@gmail.com>
2023-05-26 23:24:11 -0600

    creación de dashboard de superadmin

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -2,10 +2,10 @@
 @section('contenido')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-6 mt-5">
-                <form action="" class="form-control mt-5 colorForms py-3 text-center" method="POST">
+            <div class="col-4 mt-5">
+                <form action="" class="form-control mt-5 colorForms py-3 text-center shadow-lg" method="POST">
                     <label for="user" class="text-light fs-3">Usuario</label>
-                    <input type="text" class="form-control" name="user" id="user" autofocus>                    
+                    <input type="text" class="form-control rounded" name="user" id="user" autofocus>                    
                     <label for="user" class="text-light fs-3 mt-3">Contraseña</label>
                     <input type="password" class="form-control mb-4" name="user" id="user">
                     <div class="row">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthLogin;
+use App\Http\Controllers\SuperAdmin;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[AuthLogin::class,'index'])->name('login');
+
+Route::controller(SuperAdmin::class)->group(function(){
+    Route::get('/dashSadm','index')->name('inicio-admin');
+});
