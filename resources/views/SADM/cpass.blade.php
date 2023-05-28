5ec42be44c715c3d860f60614f49330deefddfe4
Danx398 <danidannniel398@gmail.com>
2023-05-27 23:04:19 -0600

    creación de cambio contraseña superadmin

---
+++ resources/views/SADM/cpass.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts/main')
+@section('contenido')
+    @include('shared/nav')
+    <div class="container">
+        <h1 class="text-center mb-4 mt-3">Cambiar contraseña </h1>
+        <div class="row justify-content-center mt-3">
+            <div class="col-2"></div>
+            <div class="col-8">
+                <div class="bg-primary pt-5 pb-5 text-light text-center fs-1 rounded-5">Datos del usuario</div>
+            </div>
+            <div class="col-2"></div>
+        </div>
+        <form action="">
+            <div class="row justify-content-between text-center mt-5">
+                <div class="col-4">
+                    <label for="" class="fs-4">Contraseña Nueva</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-4"></div>
+                <div class="col-4">
+                    <label for="" class="fs-4">Confirmar Contraseña</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-2">
+                        <button class="btn btn-primary container-fluid pt-4 pb-4 rounded-4 fs-3 mt-5">Cambiar</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+@endsection
