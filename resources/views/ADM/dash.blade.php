eba12716be82bd198a6ef60be175add91a01618a
JossRV <jossrv74@gmail.com>
2023-05-28 00:30:20 -0600

    ADM y dash

---
+++ resources/views/ADM/dash.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts/main')
+@section('contenido')
+    @include('shared/nav')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col">
+                <div class="row justify-content-between">
+                    <div class="col-2"></div>
+                    <div class="col-2 mt-5">
+                        <button class="btn btn-primary container-fluid pt-4 pb-4">
+                            <div class="row mb-3">
+                                <div class="col">
+                                    {{-- <img src="{{asset('img/usuario1.png')}}" alt="" class="w-25"> --}}
+                                    <i class="fa-solid fa-user fa-2xl"></i>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    Registrar Alumno
+                                </div>
+                            </div>
+                        </button>
+                    </div>
+                    <div class="col-2 mt-5">
+                        <button class="btn btn-primary container-fluid pt-4 pb-4">
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <i class="fa-solid fa-list-ul fa-2xl"></i>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    Creditos en tramite
+                                </div>
+                            </div>
+                        </button>
+                    </div>
+                    <div class="col-2 mt-5">
+                        <button class="btn btn-primary container-fluid pt-4 pb-4">
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <i class="fa-solid fa-list-check fa-2xl"></i>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    Creditos liberados
+                                </div>
+                            </div>
+                        </button>
+                    </div>
+                    <div class="col-2"></div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col">
+                <table class="table text-center table-bordered table-striped text-light">
+                    <thead class="bg-primary">
+                        <th>Nombre</th>
+                        <th>Numero de control</th>
+                        <th>Fecha de nacimiento</th>
+                        <th>Carrera</th>
+                        <th>+</th>
+                        <th>Editar</th>
+                        <th>Eliminar</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin;
 use App\Http\Controllers\AuthLogin;
 use App\Http\Controllers\SuperAdmin;
 use Illuminate\Support\Facades\Route;
@@ -9,4 +10,8 @@ Route::get('/',[AuthLogin::class,'index'])->name('login');
 Route::controller(SuperAdmin::class)->group(function(){
     Route::get('/dashSadm','index')->name('inicio-admin');
     Route::get('/cambiarPass','cambiarPass')->name('cambiarPass-sadmin');
+});
+
+Route::controller(Admin::class)->group(function(){
+    Route::get('/dashAdm','index')->name('admin');
 });
