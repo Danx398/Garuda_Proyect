6d04f21f1117038134460d2844b53e7f93cc0f6f
Danx398 <danidannniel398@gmail.com>
2023-06-01 19:29:51 -0600

    implementación de pdf con variables y request

---
+++ app/Http/Controllers/PdfController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PDF;
+
+class PdfController extends Controller
+{
+    public function generarPdf(Request $request)
+    {
+        /* $profesor = 'Aquino Segura Roldan';
+        $rol = 'JEFE DEL DEPARTAMENTO DE ACTIVIDADES';
+        $ambito = 'EXTRAESCOLARES'; */
+        $data = [
+            'num_control'=>$request->numero_control,
+            'anio_actual'=> date('Y'),
+            'nombre' =>$request->nombre,
+            'actividad' =>$request->actividad,
+            'fecha' => date('Y-m-d'),
+            'carrera' => $request->carrera,
+            'profesor' => ' ing. Aquino Segura Roldan',
+            'rol' => 'JEFE DEL DEPARTAMENTO DE ACTIVIDADES',
+            'ambito' => 'EXTRAESCOLARES',
+            'firma' => '________________________________'
+        ];
+        $pdf = PDF::loadView('pdf', $data);
+        // compact('profesor', 'rol', 'ambito')
+
+        return $pdf->stream('ejemplo.pdf');
+    }
+}
